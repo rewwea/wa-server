f762395098672b0345eb8cf0915a1e36b189532e
rewwea <avandronov10@gmail.com>
2023-11-29 01:20:55 +0500

    Первый контроллер

---
+++ app/auth/auth.routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+import { authUser } from './auth.controller.js'
+
+const router = express.Router()
+
+router.route('/login').post(authUser)
+
+export default router
---
+++ server.js
@@ -0,0 +1,24 @@
+import 'colors'
+import express from 'express'
+import authRoutes from './app/auth/auth.routes.js'
+
+const app = express()
+
+async function main() {
+	/* if (process.env.NODE_ENV === 'development') app.use(morgan('dev')) */
+
+	app.use(express.json())
+	app.use('api/auth', authRoutes)
+
+	const PORT = 5000
+
+	app.listen(
+		PORT,
+		console.log(
+			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+				.bold
+		)
+	)
+}
+
+main()
