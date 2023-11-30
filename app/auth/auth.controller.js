318d07af7e40efa929c14ffaaf721e6737c962f9
rewwea <avandronov10@gmail.com>
2023-11-30 21:06:34 +0500

    База данных PostgreSQL + Prisma

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,6 +1,7 @@
+import asyncHandler from 'express-async-handler'
 //@desc Auth user
 //@route POST /api/auth/login
 //@access Public
-export const authUser = async (req, res) => {
+export const authUser = asyncHandler(async (req, res) => {
 	res.json({ message: 'You are authenticated' })
-}
+})
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 import 'colors'
+import dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 import authRoutes from './app/auth/auth.routes.js'
+
+dotenv.config()
 
 const app = express()
 
 async function main() {
-	/* if (process.env.NODE_ENV === 'development') app.use(morgan('dev')) */
+	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 	app.use(express.json())
-	app.use('api/auth', authRoutes)
+	app.use('/api/auth', authRoutes)
 
-	const PORT = 5000
+	const PORT = process.env.PORT || 5000
 
 	app.listen(
 		PORT,
 		console.log(
-			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
 				.bold
 		)
 	)
